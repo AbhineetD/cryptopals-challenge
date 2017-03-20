8555698a5bf5d8d23f3fe4b604e5e8c597f39cab
AbhineetD <abhineet.d@gmail.com>
2017-03-21 01:52:17 +0530

    Set 1 Challenge 3

---
+++ set1/challenge3.py
@@ -0,0 +1,48 @@
+#Cryptopals Challenge Set 1 Challenge 3
+
+import codecs
+dictionary = ['a', 'an', 'in', 'on', 'of', 'the']
+
+'''
+This function gives XOR of 2 hex strings 
+@Input: 2 Hex strings of equal length
+@Output: XOR output of 2 strings
+'''
+def xor_2_strings(hex_string_1, hex_string_2):
+	if (len(hex_string_1) == len(hex_string_2)):
+		return hex(int(hex_string_1, 16) ^ int(hex_string_2, 16))
+	else:
+		return 0
+
+'''
+1. Iterate from decimal 33 to 126 (ascii chars)
+2. Get the hex value of decimal 
+3. Encode string of length 34 with the hex value
+4. xor original string with generated string
+5. Print the string in character format
+6. Check for common words like a/the/an/of
+@Input: hex string
+@Output: prints all decoded strings
+'''
+
+def find_key(hex_string):
+	
+	for dec in range(33, 126):
+		generated_hex_string = ""
+		for i in range(len(hex_string)//2):
+			generated_hex_string += hex(dec)[2:]
+		xored_string = xor_2_strings(hex_string, generated_hex_string)[2:]
+		key = chr(dec)
+		decoded_string = bytearray.fromhex(xored_string).decode("utf-8")
+		for word in dictionary:
+			if word in decoded_string:
+				print("String: ", decoded_string)
+				print("Key: ", key, "\n")
+
+
+if __name__ == "__main__":
+
+	hex_string = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+	find_key(hex_string)
+
+
